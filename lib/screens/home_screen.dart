bd86d9d5a820fe55f97094dcc4b6a1d4498f90c9
SupreetRonad <62396592+SupreetRonad@users.noreply.github.com>
2021-06-23 22:22:56 +0530

    HomeScreen UI done v0.1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,35 +35,35 @@ class HomeScreen extends StatelessWidget {
   Widget _centerStack(BuildContext context) => Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _stack1,
-          _stack2,
+          _header,
+          _displayInfo(),
         ],
       );
 
-  Widget _stack1 = Stack(
+  Widget _header = Stack(
     alignment: Alignment.center,
     children: [
       Padding(
         padding: const EdgeInsets.only(
-          top: 70,
+          top: 90,
         ),
         child: Card(
-          elevation: 5,
+          shadowColor: Colors.white,
+          elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(50, 40, 50, 10),
+            padding: const EdgeInsets.fromLTRB(40, 40, 40, 10),
             child: Text(
               'TDS Returns Made Easy',
               style: TextStyle(
-                fontSize: 15,
+                fontSize: 17,
                 fontWeight: FontWeight.w600,
                 color: Colors.black54,
               ),
             ),
           ),
-          
         ),
       ),
       Card(
@@ -89,16 +89,101 @@ class HomeScreen extends StatelessWidget {
     ],
   );
 
-  Widget _stack2 = Stack(
-    children: [
-      Center(
-        child: Image.asset(
-          'assets/bg.png',
-          width: 700,
+  Widget _displayInfo() => Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 100),
+            child: Image.asset(
+              'assets/bg.png',
+              width: 550,
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                width: 350,
+                height: 140,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 8,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(.5),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: _userInfo(),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                'helpline : +91-33-22875500, 9836490007',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.75),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'email : info@tdsman.com',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.75),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+
+  Widget _userInfo() => Column(
+        children: [
+          _formatText('Name', 'HUKAMI CHAND'),
+          _formatText('Serial No', 'A7VC7LRH'),
+          _formatText('Financial year', '2005-06 TO 2020-21'),
+          _formatText('Version date', '29 APRIL 2021'),
+        ],
+      );
+
+  Widget _formatText(String header, String value) => Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              child: Row(
+                children: [
+                  _styledText(header),
+                  Spacer(),
+                  _styledText(':'),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            _styledText(value),
+          ],
         ),
-      ),
-    ],
-  );
+      );
+
+  Widget _styledText(String text) => Text(
+        text,
+        style: TextStyle(
+          color: Colors.black.withOpacity(.8),
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+        ),
+      );
 
   Widget _sideMenu() => Container(
         color: Colors.blue[700],
